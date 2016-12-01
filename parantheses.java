d4b41d26bcb1509474d0133c86c66017020977e6
Robinroy peter <p.v.robinroy@gmail.com>
2016-12-01 14:34:18 +0530

    Add files via upload

---
+++ parantheses.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class parantheses{
+	public static void main(String args[]){
+		String str ="({{[(]}})";
+		boolean balnced = true;
+		myStack stack_one = new myStack(str.length());
+		for(int i=0;i<str.length();i++){
+			char myChar =str.charAt(i);
+			if(myChar ==')'){
+				if(stack_one.Pop() !='('){
+					balnced = false;
+				}			
+			}else if(myChar =='}'){
+				if(stack_one.Pop() !='{'){
+					balnced = false;
+				}
+			}else if(myChar ==']'){
+				if(stack_one.Pop() !='['){
+					balnced = false;
+				}	
+			}else{stack_one.Push(myChar);
+				}
+		}
+		System.out.println(balnced);
+	}
+}
