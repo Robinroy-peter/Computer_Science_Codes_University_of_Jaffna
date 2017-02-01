1c31453f9834cbcd804c6ae5308b9e93d65de8ac
Robinroy peter <p.v.robinroy@gmail.com>
2017-02-01 22:06:25 +0530

    Update powerset.java

--- powerset.java
+++ powerset.java
@@ -1,22 +1,32 @@
 import java.util.*;
 public class powerset{
-    public static void perm(String a[],int k,int n){
-		if(k==n){
-			System.out.println("{ }");
-			//return "";
-		}else{
-			perm(a,k+1,n);
-			
-			for(int u=k+1;u<n;u++){
-			System.out.println(a[u-1].concat(perm(a,k+1,n))+",");
-			}
-			return a[k];
+	int c;
+	int z;
+    public  void perm(String a[],int k,int n,String b[]){
+    	if(k==n-1){
+		b[0] =" ";
+		b[1] =a[k];
+		System.out.println("{ }");
+		System.out.println(a[k]);
+		c=2;
+    	}else {
+		perm(a,k+1,n,b);
+		z=c;
+		for(int y=0;y<z;y++){
+			String s =a[k].concat(b[y]);
+			b[c] =s;
+			System.out.println(s);	
+			c++;		
 		}
+	}
     }
-
     public static void main(String[] args){
         powerset one =new powerset();
-        String a[] ={"A","B","C"};
-        one.perm(a,0,3);
+
+        String a[] ={"A","B","C","D","E"};
+        int d =3;
+         String[] q=new String[(int)Math.pow(2,d)];
+        one.perm(a,0,d,q);
+
     }
 }
